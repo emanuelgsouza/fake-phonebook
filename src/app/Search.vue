332a954f1ae823c4be611a02b43acd6d3e8b77e9
Emanuel Gonçalves <emanuelgdesouza@gmail.com>
2017-02-07 11:02:57 -0200

    Update search and contact components

--- src/app/Search.vue
+++ src/app/Search.vue
@@ -8,11 +8,24 @@
         :invalid="errors.has('nome')"
         label="Nome"
         type="text"
-        placeholder="Digite um nome para pesquisa" v-model="name"></ui-textbox>
+        placeholder="Digite um nome para pesquisa"
+        v-model="name"></ui-textbox>
+      <hr>
+      <transition name="fade">
+        <ui-button
+          v-if="selecteds.length !== 0"
+          type="primary"
+          color="red"
+          icon="delete"
+          iconPosition="left"
+          size="large"
+          @click="deleteSelecteds"> Deletar Selecionados </ui-button>
+      </transition>
       <hr>
       <Contact
         v-for="contact in contacts"
-        :contactObject="contact"></Contact>
+        :contactObject="contact"
+        @changeValue="changeSelecteds"></Contact>
     </div>
     <HomeMenu></HomeMenu>
     <FooterHome></FooterHome>
@@ -27,16 +40,50 @@ import Contact from './components/Contact'
 import FooterHome from './components/Footer'
 
 export default {
-  name: 'add',
+  name: 'search',
   components: { HomeHeader, Contact, HomeMenu, FooterHome },
   data () {
     return {
       name: '',
-      contacts: []
+      contacts: [],
+      contactsCopy: [],
+      selecteds: []
     }
   },
-  methods: {},
-  mounted () { this.contacts = returnAllContacts() }
+  methods: {
+    changeSelecteds () {
+      if (arguments[0]) {
+        this.selecteds.push(arguments[1])
+      } else {
+        const pos = this.selecteds.indexOf(arguments[1])
+        this.selecteds.splice(pos, 1)
+      }
+    },
+    deleteSelecteds () {
+      const contactsFilter = this.contacts.filter(contact => {
+        return this.selecteds.indexOf(contact.Nome) === -1
+      })
+      this.selecteds = []
+      this.contacts = contactsFilter
+    }
+  },
+  watch: {
+    name () {
+      const name = arguments[0]
+      if (name !== '') {
+        const result = this.contactsCopy.filter(contact => {
+          return contact.Nome.indexOf(name) !== -1
+        })
+        this.contacts = result
+      } else {
+        this.contacts = this.contactsCopy
+      }
+    }
+  },
+  mounted () {
+    this.contacts = returnAllContacts()
+    this.contactsCopy = this.contacts
+  }
 }
 </script>
 
@@ -44,4 +91,15 @@ export default {
   div.content
     font-size: 1.2em
     padding: .5em .8em
+
+  .fade-enter-active
+    transition: all .3s ease
+
+  .fade-leave-active
+    transition: all .3s ease
+
+  .fade-enter, .fade-leave-to
+    transform: translateX(-100%)
+    opacity: 0
+
 </style>
--- src/app/components/Contact.vue
+++ src/app/components/Contact.vue
@@ -5,10 +5,25 @@
       :username="contactObject.Nome"
       :src="contactObject.avatar"></Avatar>
     <p> {{ contactObject.Nome }} </p>
+    <ui-fab
+      class="btn-info"
+      color="primary"
+      icon="info"
+      size="small"
+      @click="$refs['modal'].open()"></ui-fab>
     <ui-checkbox
       class="checkbox"
       v-model="value"
       ></ui-checkbox>
+
+    <ui-modal
+      ref="modal"
+      title="Mostrando os dados do contato:"
+      transition="scale">
+      <ul>
+        <li v-for="(value, key) in contactObject"> {{ key }} - {{ value }} </li>
+      </ul>
+    </ui-modal>
   </div>
 </template>
 
@@ -19,7 +34,16 @@ export default {
   name: 'contact-td',
   data () { return { value: [] } },
   components: { Avatar },
-  props: ['contactObject']
+  props: ['contactObject'],
+  watch: {
+    value () {
+      if (this.value) {
+        this.$emit('changeValue', true, this.contactObject.Nome)
+      } else {
+        this.$emit('changeValue', false, this.contactObject.Nome)
+      }
+    }
+  }
 }
 </script>
 
@@ -30,7 +54,7 @@ export default {
     margin: .5em 0
     padding: .4em .5em
     border-radius: 2px
-    box-shadow: 1px 1px 10px black
+    box-shadow: .5px .5px 5px black
 
     & p
       position: absolute
@@ -41,4 +65,9 @@ export default {
       position: absolute
       top: 1.3em
       right: 1.5em
+
+    & .btn-info
+      position: absolute
+      top: 1em
+      right: 4em
 </style>
